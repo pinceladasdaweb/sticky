5d19dcff1e7a405dbb794b6f17785c72bdf9674c
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-10-28 18:32:08 -0200

    Update tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,33 @@
 module.exports = function (grunt) {
-    pkg: grunt.file.readJSON('package.json'),
+    "use strict";
+
+    var pkg  = grunt.file.readJSON("package.json"),
+        date = new Date();
+
     grunt.initConfig({
+        meta: {
+            banner: '/*! ' + pkg.name + ' ' + pkg.version + ' | (c) ' + date.getFullYear() + ' ' + pkg.author + ' | ' + pkg.licenses[0].type + ' License */'
+        },
         uglify: {
             options: {
-                preserveComments: 'all'
+                banner: '<%= meta.banner %>\n'
             },
-            src: {
+            target: {
                 files: {
-                    'assets/js/sticky.min.js': ['assets/js/sticky.js']
+                    'build/sticky.min.js': ['src/sticky.js']
                 }
-            },
+            }
+        },
+        watch: {
+            js: {
+                files: ['src/sticky.js'],
+                tasks: ['uglify']
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', [ 'uglify' ]);
+    grunt.registerTask('default', [ 'uglify', ]);
 };
